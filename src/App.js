d2e414b65e35e19b33548041c9786d639bf74170
liuZongYi <er345ssdlh@163.com>
2021-04-13 10:25:35 +0800

    结构

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@
  * @Author: lzy
  * @Date: 2021-04-12 13:53:50
  * @LastEditors: Andy
- * @LastEditTime: 2021-04-12 17:01:40
+ * @LastEditTime: 2021-04-13 10:13:43
  */
 
 import React, { Component } from 'react';
-// 状态管理
-import { Provider } from 'react-redux';
-// 引入store
-import store from './store/index'
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
+import 'antd/dist/antd.css';
+import { ConfigProvider, DatePicker, message } from 'antd';
 // 子组件
 // import Text from './pages/Test'
 // import Hello from './components/hello'
@@ -20,14 +20,19 @@ import Demo1 from './components/demo1/TestFar'
 class App extends Component {
   render() {
     return (
-      // Provider是react-redux两个核心工具之一，作用：将store传递到每个项目中的组件中
-      <Provider store={store}>
         <div className="App">
             {/* <Text /> */}
             {/* <Hello /> */}
             <Demo1 />
+            <ConfigProvider locale={zhCN}>
+              <div style={{ width: 400, margin: '100px auto' }}>
+                <DatePicker onChange={handleChange} />
+                <div style={{ marginTop: 16 }}>
+                  当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+                </div>
+              </div>
+            </ConfigProvider>
         </div>
-      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@
  * @Author: lzy
  * @Date: 2021-04-12 11:28:32
  * @LastEditors: Andy
- * @LastEditTime: 2021-04-12 13:53:37
+ * @LastEditTime: 2021-04-13 10:14:15
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+// 状态管理 Provider是react-redux两个核心工具之一，作用：将store传递到每个项目中的组件中
+import { Provider } from 'react-redux';
+// 引入store
+import store from './store/index'
 import App from './App'
-ReactDOM.render(
-    <App />
-    , document.getElementById("root"))
+
+const AppView = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+ReactDOM.render(AppView, document.getElementById("root"))
